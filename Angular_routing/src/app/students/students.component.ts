4cc01f310b06cabc1d0067e1fc5c52329bfa5eb9
YaminiMajeti <mvnagayamini17@gmail.com>
2024-10-07 15:37:39 +0530

    filter_students

--- Angular_routing/src/app/students/students.component.ts
+++ Angular_routing/src/app/students/students.component.ts
@@ -9,7 +9,28 @@ import { StudentService } from '../student.service';
 export class StudentsComponent {
   constructor(private studservice:StudentService){}
   students=this.studservice.students;
+  filterStudents=this.students;
 
   totalMarks=100;
-  filterText:any;
+  _filterText:any;
+
+  get filterText(){
+    return this._filterText;
+  }
+  set filterText(value:string){
+      this._filterText=value;
+      this.filterStudents=this.filterByGender(value);
+  }
+
+  filterByGender(filterTerm:string){
+    if(this.students.length==0 || filterTerm==''){
+      return this.students;
+    }else{
+      return this.students.filter((student)=>
+      {
+        return student.gender.toLowerCase()==filterTerm.toLowerCase();
+      })
+    }
+  }
+
 }
