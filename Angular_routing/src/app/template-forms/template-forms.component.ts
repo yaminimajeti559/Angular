32c6c16f815fc45d98a17541f1dcdac9ad18e77d
YaminiMajeti <mvnagayamini17@gmail.com>
2024-10-08 14:34:35 +0530

    Validations

--- Angular_routing/src/app/template-forms/template-forms.component.ts
+++ Angular_routing/src/app/template-forms/template-forms.component.ts
@@ -11,7 +11,35 @@ export class TemplateFormsComponent {
     firstname:string | undefined;
     lastName:string|undefined;
     emailid:string|undefined;
+    country:string|undefined;
+    gen:string|undefined;
+    gender=[
+      {id:'1',value:'Male'},
+      {id:'2',value:'female'}
+    ];
     onSubmit(){
       console.log(this.form);
+
+      this.firstname=this.form.value.personalDetails.firstname;
+      this.lastName=this.form.value.personalDetails.lastName;
+      this.emailid=this.form.value.personalDetails.emailid;
+      this.gen=this.form.value.gender;
+      this.country=this.form.value.country;
+
+      // this.form.reset();
+    }
+    setDefault(){
+      // this.form.value.personalDetails.firstname='john';
+      // this.form.value.personalDetails.lastName='David';
+      // this.form.value.personalDetails.emailid='john@gmail.com';
+      this.form.setValue({
+        // country: '',
+        gender: '',
+        personalDetails:{
+          firstname:'John',
+          lastName:'smith',
+          emailid:'abc@gmail.com'
+        }
+      })
     }
 }
